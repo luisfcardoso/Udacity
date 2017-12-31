fd7aaadd0408290470bc94a8945d933603c1c7c5
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2017-12-31 21:11:56 -0200

    subindo exercício

---
+++ Fundamentos de Data Science 1/Semana2-Aula5-09.py
@@ -0,0 +1,12 @@
+def create_cast_list(filename):
+    cast_list = []
+    # usar with para abrir o arquivo filename
+    with open(filename) as f:
+    	# use a sintaxe do laço for para processar cada linha
+    	# e adicione o nome do ator a cast_list
+        for line in f:
+            line_data = line.split(',')
+            cast_list.append(line_data[0])
+    	return cast_list
+
+print create_cast_list('flying_circus_cast.txt')
