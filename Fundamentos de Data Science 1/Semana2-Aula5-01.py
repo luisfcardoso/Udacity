4c40010ed1542f130c83e7446d1f09078c1ac2bb
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2017-12-31 00:09:04 -0200

    Subindo alguns exercícios

---
+++ Fundamentos de Data Science 1/Semana2-Aula5-01.py
@@ -0,0 +1,2 @@
+def hours2days(hour):
+    return int(hour/24), int(hour%24)
---
+++ Fundamentos de Data Science 1/Semana2-Aula5-03.py
@@ -0,0 +1,14 @@
+def print_list(l, numbered=False, bullet_character='*'):   #Argumentos padrões => fica optativo e entra o valor default
+    """Prints a list on multiple lines, with numbers or bullets
+    
+    Arguments:
+    l: The list to print
+    numbered: set to True to print a numbered list
+    bullet_character: The symbol placed before each list element. This is
+                      ignored if numbered is True.
+    """
+    for index, element in enumerate(l):
+        if numbered:
+            print("{}: {}".format(index+1, element))
+        else:
+            print("{} {}".format(bullet_character, element))
